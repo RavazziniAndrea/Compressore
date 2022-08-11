5124e3d7ba8bdde167bacc5d48d99e136b2b6b6f
andrea <andrea.ravazzini@horsa.it>
2022-08-11 15:31:17 +0200

    added log_file

--- compressore.py
+++ compressore.py
@@ -2,6 +2,7 @@ import os
 import json
 import subprocess
 import time
+from datetime import datetime as dt
 from getpass import getpass
 from termcolor import colored as col
 
@@ -21,8 +22,11 @@ def start():
         print(col("Error reading params. Abort", "red"))
         return -1
 
-    if check_conn_ssh() == -1: return -1
-    PASSW = read_psw()
+    getLogFile()
+
+    if SSH_ENABLED:
+        if check_conn_ssh() == -1: return -1
+        PASSW = read_psw()
 
     cart = select_folders()
     if len(cart) < 1:
@@ -34,9 +38,10 @@ def start():
     print(col("* Backup:", "cyan"))
     for c in cart:
         subprocess.call(["/bin/du","-sh", c], shell=False)
+        subprocess.call(["/bin/du","-sh", c], stdout=f_log, shell=False) #brutto ma non so come scrivere contemporaneamente su f e stdout
 
     print(col("* Backup saved temporarily in "+SAVEFOLDER+"", "cyan"))
-    print(col("* SCP to "+IP_SSH+"", "cyan"))
+    print(col(("* SCP to "+IP_SSH) if SSH_ENABLED else "* No SSH", "cyan"))
     print(col("* Shutdown at the end? " +("yes" if shut == 1 else "no")+ "\n", "cyan"))
 
     inp = input(col("Do you want to proced? [y/n] ", "cyan"))
@@ -49,27 +54,34 @@ def start():
     for c in cart:
         nome_tar = SAVEFOLDER+"/"+c.split("/")[-1]+".tar"
         nome_tar_bz2 = nome_tar+".bz2"
-        res = subprocess.call(["/bin/tar", "-cvf", nome_tar, c], shell=False)
-        res = subprocess.call(["/bin/pbzip2", "-v","-f", "-p4", nome_tar], shell=False)
+        start_time = dt.now()
+        printer("\nStarting compression: {}".format(start_time), "white")
+        printer("Compression "+SAVEFOLDER+"/"+c.split("/")[-1]+"...", "white")
+        res = subprocess.call(["/bin/tar", "-cvf", nome_tar, c], stdout=f_log, stderr=f_log ,shell=False)
+        res = subprocess.call(["/bin/pbzip2", "-v","-f", "-p4", nome_tar], stdout=f_log, stderr=f_log, shell=False)
         # res = os.system("tar -I \"pbzip2 -p4\" -cvf "+nome_tar+" "+c)
         if res != 0:
             print(col("\nERROR compression "+nome_tar+" failed. Abort","red"))
             err = 1
             break
-        print(col("\n***** COMPRESSION COMPLETED *****\n","green"))
+        printer("**** COMPRESSION COMPLETED ****","green")
+        printer("Duration: {}\n".format(dt.now()-start_time),"white")
 
-        res = subprocess.call(["/bin/sshpass","-p", PASSW, "scp", "-v", nome_tar_bz2, USER_SSH+"@"+IP_SSH+":"+FOLDER_SSH], shell=False)
-        # res = os.system("sshpass -p '"+PASSW+"' scp -v "+nome_tar+" "+USER_SSH+"@"+IP_SSH+":"+FOLDER_SSH)
-        if res != 0:
-            print(col("\nERROR send file "+c.split("/")[-1]+".tar.bz2 via ssh failed. Abort","red"))
-            err=1
-            break
-        print(col("\n***** SEND SSH COMPLETED *****\n","green"))
-        subprocess.call(["/bin/rm", nome_tar_bz2], shell=False)
-        # os.system("rm "+nome_tar)
-
+        if SSH_ENABLED:
+            start_time = dt.now()
+            printer("Starting scp: {}".format(start_time), "white")
+            printer("Sending "+nome_tar_bz2+" to "+IP_SSH+":"+FOLDER_SSH+"...", "white")
+            res = subprocess.call(["/bin/sshpass","-p", PASSW, "scp", "-v", nome_tar_bz2, USER_SSH+"@"+IP_SSH+":"+FOLDER_SSH], shell=False)
+            if res != 0:
+                print(col("\nERROR send file "+c.split("/")[-1]+".tar.bz2 via ssh failed. Abort","red"))
+                err=1
+                break
+            print(col("\n***** SEND SSH COMPLETED *****\n","green"))
+            printer("Duration: {}\n".format(dt.now()-start_time),"white")
+            subprocess.call(["/bin/rm", nome_tar_bz2], shell=False)
+        
     if(err == 0):
-        print(col("\n\n*******************************", "green"))
+        print(col("\n*******************************", "green"))
         print(col("***  COMPRESSORE TERMINATED ***", "green"))
         print(col("*******************************\n", "green"))
         if shut == 1:
@@ -77,7 +89,7 @@ def start():
             time.sleep(5)
             subprocess.call(["/usr/sbin/shutdown", "now"], shell=False) 
     else:
-        print(col("\n\n*******************************", "red"))
+        print(col("\n*******************************", "red"))
         print(col("!!!!     ERROR DETECTED    !!!!", "red"))
         print(col("*******************************\n", "red"))
         return -1
@@ -86,11 +98,13 @@ def start():
 #TODO renderlo più parlabile. Per ora dice solo "Errore lettura parametri"
 def read_params(): #throw Exception
 
+    global SSH_ENABLED
     global IP_SSH
     global USER_SSH
     global FOLDER_SSH
     global SAVEFOLDER
     global FOLDERS
+    global LOG_ENABLED
     global LOG_FOLDER
 
     f = open('params.json')
@@ -104,11 +118,32 @@ def read_params(): #throw Exception
         if d == "tmp_store_folder":
             SAVEFOLDER = data["params"]["tmp_store_folder"]
         if d == "ssh":
-            IP_SSH = data["params"]["ssh"]["ip"]
-            USER_SSH = data["params"]["ssh"]["user"]
-            FOLDER_SSH = data["params"]["ssh"]["folder"]
+            SSH_ENABLED = data["params"]["ssh"]["enable"]
+            if SSH_ENABLED :
+                IP_SSH = data["params"]["ssh"]["ip"]
+                USER_SSH = data["params"]["ssh"]["user"]
+                FOLDER_SSH = data["params"]["ssh"]["folder"]
         if d == "log_folder":
-            LOG_FOLDER  = data["params"]["log_folder"]
+            LOG_ENABLED = data["params"]["log_folder"]["enable"]
+            if LOG_ENABLED:
+                LOG_FOLDER  = data["params"]["log_folder"]["path"]
+
+def getLogFile():
+
+    if not LOG_ENABLED:
+        return None
+
+    global LOG_FILE_PATH
+    global f_log
+    e=dt.now()
+    LOG_FILE_PATH = LOG_FOLDER+"/compressore_%s_%s_%s" % (e.year, e.month, e.day)
+
+    try:
+        #if the file with this date exists, it will append
+        f_log = open(LOG_FILE_PATH, "w" if not os.path.isfile(LOG_FILE_PATH) else "a")
+    except FileExistsError:
+        print("ERRORE")
+
 
 def read_psw():
     return getpass("Insert ssh password for user "+USER_SSH+": ")
@@ -147,6 +182,10 @@ def select_folders():
                     break
     return cart
 
+
+def printer(msg, color):
+    print(col(msg, color))
+    f_log.write(msg)
 
 if __name__ == "__main__":
     start()
